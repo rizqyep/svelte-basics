03b7a7b22e6b03430eb261638e69f0ca3ab8ed2b
Rizqy EP <rizqyepr@gmail.com>
2021-07-21 23:46:04 +0700

    Feat: lesson6 codes

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,19 @@
 <script>
-  let name = "Rep";
-  let beltColour = "Black";
-
-  const handleClick = () => {
-    if (beltColour == "Black") {
-      beltColour = "Orange";
-    } else {
-      beltColour = "Black";
-    }
-  };
-
-  const handleInput = (event) => {
-    beltColour = event.target.value;
-  };
+  let people = [
+    { name: "Abek", beltColour: "Black", age: 21, id: 1 },
+    { name: "Ipul", beltColour: "Orange", age: 21, id: 2 },
+    { name: "Sesar", beltColour: "Brown", age: 21, id: 3 },
+  ];
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Belt Colour : {beltColour}
-  </p>
-  <button on:click={handleClick}>Update Belt Colour</button>
-  <!-- <input type="text" on:input={handleInput} value={beltColour} /> -->
-
-  <!-- Two Way Binding ShortCut -->
-
-  <input type="text" bind:value={beltColour} />
+  {#each people as person (person.id)}
+    <div class="card">
+      <h4>{person.name}</h4>
+      <p>{person.beltColour}</p>
+      <p>{person.age}</p>
+    </div>
+  {/each}
 </main>
 
 <style>
@@ -34,6 +22,7 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+    background-color: #dfdfdf;
   }
 
   h1 {
@@ -41,6 +30,14 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+  }
+
+  .card {
+    background-color: #ffffff;
+    box-shadow: 1px 1px 10px #cecece 1px 1px 20px #cecece;
+    width: 80vw;
+    padding: 10px;
+    margin: 30px auto;
   }
 
   @media (min-width: 640px) {
