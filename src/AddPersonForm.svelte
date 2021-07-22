9924d8362437f2bdfb26ab92c668faf25bae63b4
Rizqy EP <rizqyepr@gmail.com>
2021-07-23 01:04:42 +0700

    feat: hadnle submit form function

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,13 @@
   let name;
   let beltColour;
   let age;
+
+  const handleSubmit = () => {
+    console.log(name, beltColour, age);
+  };
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <input type="text" placeholder="Name" bind:value={name} />
   <input type="text" placeholder="Belt Colour" bind:value={beltColour} />
   <input type="number" placeholder="Age" bind:value={age} />
