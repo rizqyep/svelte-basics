53f8657cc7e2c054bf3e9d61f192c4f62d371aab
Rizqy EP <rizqyepr@gmail.com>
2021-07-23 00:35:11 +0700

    feat: code lesson10

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,14 +1,34 @@
 <script>
   let showModal = true;
+  let isPromo = true;
 </script>
 
 {#if showModal}
   <div class="backdrop">
-    <div class="modal">
+    <div class="modal" class:promo={isPromo}>
       <p>Sign Up for offers!!</p>
     </div>
   </div>
 {/if}
 
 <style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    background: white;
+  }
+
+  .promo {
+    background: crimson;
+    color: white;
+  }
 </style>
